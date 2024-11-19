42969371a78b60483f33c32d919714bcaeacbc37
Akshat Desai <akshatdesai.ce@gmail.com>
2024-11-19 19:26:11 +0530

    Initial commit

---
+++ EventHubPublisher/Publish.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.EventHubs;
+
+namespace EventHubPublisher
+{
+    class Publish
+    {
+        // Azure Event Hub connection settings
+        private const string EventHubConnectionString = "connection-string";
+        private const string EventHubName = "traintracer";
+
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Azure Event Hub Publisher");
+
+            // Create Event Hub client
+            var connectionStringBuilder = new EventHubsConnectionStringBuilder(EventHubConnectionString)
+            {
+                EntityPath = EventHubName
+            };
+
+            var eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
+
+            try
+            {
+                Console.WriteLine("Enter the number of events to send:");
+                int eventCount = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i <= eventCount; i++)
+                {
+                    string message = $"Event {i} at {DateTime.Now}";
+                    Console.WriteLine($"Sending: {message}");
+
+                    var eventData = new EventData(Encoding.UTF8.GetBytes(message));
+                    await eventHubClient.SendAsync(eventData);
+                }
+
+                Console.WriteLine($"{eventCount} events successfully sent!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+            finally
+            {
+                await eventHubClient.CloseAsync();
+            }
+        }
+    }
+}
---
+++ EventHubSubscriber/Subscribe.cs
@@ -0,0 +1,61 @@
+﻿using Azure.Identity;
+using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Consumer;
+using Azure.Messaging.EventHubs.Processor;
+using Azure.Storage.Blobs;
+using System;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHubSubscriber
+{
+    class Subscribe
+    {
+
+
+        static async Task Main(string[] args)
+        {
+
+
+            BlobContainerClient storageClient = new BlobContainerClient(
+                new Uri("https://eventstoragepubsub.blob.core.windows.net/eventhub-checkpoints"),
+                new DefaultAzureCredential(includeInteractiveCredentials: false));
+            var processor = new EventProcessorClient(
+                storageClient,
+                EventHubConsumerClient.DefaultConsumerGroupName,
+                "dsbhub.servicebus.windows.net",
+                "traintracer",
+            new DefaultAzureCredential());
+
+            processor.ProcessEventAsync += ProcessEventHandler;
+            processor.ProcessErrorAsync += ProcessErrorHandler;
+
+            await processor.StartProcessingAsync();
+
+            // Wait for 30 seconds for the events to be processed
+            await Task.Delay(TimeSpan.FromSeconds(30));
+
+            // Stop the processing
+            await processor.StopProcessingAsync();
+        }
+
+        static Task ProcessEventHandler(ProcessEventArgs eventArgs)
+        {
+            // Write the body of the event to the console window
+            Console.WriteLine("\tReceived event: {0}", Encoding.UTF8.GetString(eventArgs.Data.Body.ToArray()));
+            Console.ReadLine();
+            return Task.CompletedTask;
+        }
+
+        static Task ProcessErrorHandler(ProcessErrorEventArgs eventArgs)
+        {
+            // Write details about the error to the console window
+            Console.WriteLine($"\tPartition '{eventArgs.PartitionId}': an unhandled exception was encountered. This was not expected to happen.");
+            Console.WriteLine(eventArgs.Exception.Message);
+            Console.ReadLine();
+            return Task.CompletedTask;
+        }
+
+
+    }
+}
