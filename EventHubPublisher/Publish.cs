f15ae048227b28397f344d518ee51cc53297973b
Akshat Desai <akshatdesai.ce@gmail.com>
2024-12-11 16:26:01 +0530

    Update Publish.cs

--- EventHubPublisher/Publish.cs
+++ EventHubPublisher/Publish.cs
@@ -1,15 +1,18 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
+using Newtonsoft.Json;
 
 namespace EventHubPublisher
 {
     class Publish
     {
         // Azure Event Hub connection settings
-        private const string EventHubConnectionString = "connection-string";
-        private const string EventHubName = "traintracer";
+        private const string EventHubConnectionString = "";
+        private const string EventHubName = "";
+        private const string partitionId = "1";
 
         static async Task Main(string[] args)
         {
@@ -28,16 +31,22 @@ namespace EventHubPublisher
                 Console.WriteLine("Enter the number of events to send:");
                 int eventCount = int.Parse(Console.ReadLine());
 
-                for (int i = 1; i <= eventCount; i++)
-                {
-                    string message = $"Event {i} at {DateTime.Now}";
-                    Console.WriteLine($"Sending: {message}");
+                List<TrainData> trainDataList =  PrepareData.CreateTelemetryData();
 
-                    var eventData = new EventData(Encoding.UTF8.GetBytes(message));
-                    await eventHubClient.SendAsync(eventData);
+                
+
+                foreach (var trainData in  trainDataList)
+                {
+                   
+                    Console.WriteLine($"Sending: {trainData}");
+                    string json = JsonConvert.SerializeObject(trainData);
+
+                    var eventData = new EventData(Encoding.UTF8.GetBytes(json));
+                    await eventHubClient.SendAsync(eventData, partitionId);
                 }
 
                 Console.WriteLine($"{eventCount} events successfully sent!");
+                int eventCount1 = int.Parse(Console.ReadLine());
             }
             catch (Exception ex)
             {
@@ -49,4 +58,6 @@ namespace EventHubPublisher
             }
         }
     }
+
+ 
 }
